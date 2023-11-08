a101aaa505a31139c1a1b620bfb2791599931770
Munish <hmunish5@gmail.com>
2023-11-08 17:14:19 +0200

    Add helmet & cors to set correct request headers & allow cross-origin requests

--- app.js
+++ app.js
@@ -1,15 +1,73 @@
-// configuring environvenment variables
-require("dotenv").config();
+const fs = require('fs');
+const path = require('path');
 
-const express = require("express");
-const bodyParser = require("body-parser");
+// configuring environvenment variables
+require('dotenv').config();
+
+const express = require('express');
+const helmet = require('helmet');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+
+const sequelize = require('./utilities/database');
 
 const app = express();
+
+// Sets required headers
+app.use(helmet());
+
+// Allow cors origin requests
+app.use(cors());
 
 // Parsing request body JSON data
 app.use(bodyParser.json());
 
-// if request does not matches any route 404 response sent
-app.use("/", (req, res) => res.status(404).send({ message: "Path not found" }));
+// create a write stream for error logs (in append mode)
+const errorLogStream = fs.createWriteStream(
+  path.join(__dirname, 'logs/error.log'),
+  {
+    flags: 'a',
+  },
+);
 
-app.listen(3000);
+// setup the logger for error
+app.use(
+  morgan('combined', {
+    skip(req, res) {
+      return res.statusCode < 400;
+    },
+    stream: errorLogStream,
+  }),
+);
+
+// create a write stream for success logs (in append mode)
+const accessLogStream = fs.createWriteStream(
+  path.join(__dirname, 'logs/access.log'),
+  {
+    flags: 'a',
+  },
+);
+
+// setup the logger for success logs
+app.use(
+  morgan('combined', {
+    skip(req, res) {
+      return res.statusCode > 400;
+    },
+    stream: accessLogStream,
+  }),
+);
+
+// if request does not matches any route 404 response sent
+app.use('/', (req, res) => res.status(404).send({ message: 'Path not found' }));
+
+// Creating connection with the database & listen on port for requests
+sequelize
+  .sync()
+  .then(() => {
+    app.listen(process.env.PORT || 3000);
+  })
+  .catch((err) => {
+    throw err;
+  });
